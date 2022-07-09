9b4e216ddfdbfc6106a56b9d9b7f71e1ab13689d
ngocphat03 <ngonngocphat@gmail.com>
2022-07-09 13:22:07 +0700

    finish homework

--- introduce-myself/introduce-mysefl.js
+++ introduce-myself/introduce-mysefl.js
@@ -11,3 +11,12 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 } 
+function playSound() {
+  const audio = new Audio('audio.mp3');
+  audio.play();
+}
+const test001 = document.querySelector('#test001');
+test001.onclick = () => {
+  playSound();
+  test001.style.top = '-50px';
+}
--- javaScript/lesson1.js
+++ javaScript/lesson1.js
@@ -1,1 +1,66 @@
 console.log('hmm')
+
+const contener1 = document.getElementsByClassName('contener1');
+contener1[0].style.backgroundColor = "blue";
+contener1[0].style.color = "yellow";
+
+const contener2 = document.getElementsByClassName('contener2');
+contener2[0].style.backgroundColor = "yellow";
+contener2[0].style.color = "blue";
+
+const p = document.getElementsByTagName('p');
+// p.style.text-align = 'center';
+
+
+var change = document.getElementById('change');
+
+change.onclick = () => {
+    var a = Math.random();
+    a *= 3;
+    var a = Math.floor(a);
+
+switch(a){
+    case a = 0:
+        contener1[0].style.backgroundColor = "yellow";
+        contener1[0].style.color = "blue";
+
+        contener2[0].style.backgroundColor = "blue";
+        contener2[0].style.color = "yellow";
+        break
+    case a = 1:
+        contener2[0].style.backgroundColor = "yellow";
+        contener2[0].style.color = "blue";
+
+        contener1[0].style.backgroundColor = "blue";
+        contener1[0].style.color = "yellow";
+        break;
+    case a = 2:
+        contener2[0].style.backgroundColor = "black";
+        contener2[0].style.color = "white";
+
+        contener1[0].style.backgroundColor = "white";
+        contener1[0].style.color = "black";
+}
+
+    // var b = 10;
+    // if(b % 2 == 0){
+    //     contener1[0].style.backgroundColor = "yellow";
+    //     contener1[0].style.color = "blue";
+
+    //     contener2[0].style.backgroundColor = "blue";
+    //     contener2[0].style.color = "yellow";
+    // }
+    // else{
+    //     contener2[0].style.backgroundColor = "yellow";
+    //     contener2[0].style.color = "blue";
+
+    //     contener1[0].style.backgroundColor = "blue";
+    //     contener1[0].style.color = "yellow";
+    // }
+    console.log(a);
+    // contener1[0].style.backgroundColor = "yellow";
+    // contener1[0].style.color = "blue";
+
+    // contener2[0].style.backgroundColor = "blue";
+    // contener2[0].style.color = "yellow";
+}
